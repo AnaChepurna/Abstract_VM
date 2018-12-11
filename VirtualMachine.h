904c3f37e3d924bec50b5b7e408066aa4fd3be48
Anastasia CHEPURNA <achepurn@e3r3p1.unit.ua>
2018-12-11 16:56:12 +0200

    first of all

---
+++ VirtualMachine.h
@@ -0,0 +1,15 @@
+//
+// Created by Anastasia CHEPURNA on 12/11/18.
+//
+
+#ifndef ABSTRACT_VM_VIRTUALMACHINE_H
+#define ABSTRACT_VM_VIRTUALMACHINE_H
+
+#include <iostream>
+
+class VirtualMachine {
+
+};
+
+
+#endif //ABSTRACT_VM_VIRTUALMACHINE_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "VirtualMachine.h"
+
+int main()
+{
+	VirtualMachine virtualMachine = VirtualMachine();
+	return (0);
+}
